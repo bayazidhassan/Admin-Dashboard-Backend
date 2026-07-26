d03540ebb9c2cb1e5d1ef907320e23c888f3e1e6
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-27 05:00:21 +0600

    user model

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import app from './app';
+import prisma from './lib/prisma';
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
+    await prisma.$connect();
+    console.log('✅ PostgreSQL connected');
+
     app.listen(PORT, () =>
       console.log(
         `Admin Dashboard server is running on port ${process.env.PORT}`,
