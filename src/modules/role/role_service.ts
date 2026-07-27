65dc4b60dab03c5fa6c3003808175749777cdb07
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-27 10:30:27 +0600

    getRoleById service

--- src/modules/role/role_service.ts
+++ src/modules/role/role_service.ts
@@ -1,3 +1,4 @@
+import AppError from '../../errors/AppError';
 import prisma from '../../lib/prisma';
 
 const createRole = async (payload: {
@@ -25,6 +26,22 @@ const createRole = async (payload: {
   return role;
 };
 
+const getRoleById = async (id: string) => {
+  const role = await prisma.role.findUnique({
+    where: { id },
+    include: {
+      permissions: true,
+    },
+  });
+
+  if (!role) {
+    throw new AppError(404, 'Role not found');
+  }
+
+  return role;
+};
+
 export const RoleService = {
   createRole,
+  getRoleById,
 };
