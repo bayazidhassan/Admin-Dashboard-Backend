d712558ba0dd490dad76944d16464cc95bb9f577
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-29 04:19:34 +0600

    createBrandValidationSchema

---
+++ src/modules/brand/brand_validation.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const createBrandValidationSchema = z.object({
+  body: z.object({
+    name: z.string().trim().min(1, 'Name is required'),
+
+    slug: z.string().trim().min(1, 'Slug is required'),
+
+    logo: z.string().trim().optional(),
+
+    status: z.boolean().optional(),
+
+    description: z.string().trim().optional(),
+  }),
+});
