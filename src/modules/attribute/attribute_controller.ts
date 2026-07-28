ba010bd0ab635cea0e72dd7fbaaa6c22e77a2f6c
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-29 04:47:00 +0600

    createAttribute controller

---
+++ src/modules/attribute/attribute_controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AttributeService } from './attribute_service';
+
+const createAttribute = catchAsync(async (req: Request, res: Response) => {
+  const result = await AttributeService.createAttribute(req.body);
+
+  sendResponse({
+    res,
+    statusCode: 201,
+    success: true,
+    message: 'Attribute created successfully',
+    data: result,
+  });
+});
+
+export const AttributeController = {
+  createAttribute,
+};
