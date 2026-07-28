29f7e875c3b95da3016d436a68e32bd5f80e8ad1
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-29 04:23:40 +0600

    createBrand route

---
+++ src/modules/brand/brand_route.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import authenticate from '../../middlewares/authenticate';
+import authorize from '../../middlewares/authorize';
+import validateRequest from '../../middlewares/validateRequest';
+import { BrandController } from './brand_controller';
+import { createBrandValidationSchema } from './brand_validation';
+
+const router = Router();
+
+router.post(
+  '/',
+  authenticate,
+  authorize('brand:create'),
+  validateRequest(createBrandValidationSchema),
+  BrandController.createBrand,
+);
+
+export const BrandRoutes = router;
