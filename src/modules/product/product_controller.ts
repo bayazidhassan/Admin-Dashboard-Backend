4af918fa5ede3cb670eba5c62eff095dc62f2ce7
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-29 05:51:19 +0600

    createProduct controller

---
+++ src/modules/product/product_controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { ProductService } from './product_service';
+
+const createProduct = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProductService.createProduct(req.body);
+
+  sendResponse({
+    res,
+    statusCode: 201,
+    success: true,
+    message: 'Product created successfully',
+    data: result,
+  });
+});
+
+export const ProductController = {
+  createProduct,
+};
