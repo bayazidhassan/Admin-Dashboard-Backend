d52674d6ac7f5a128cc66763ca6fc5d741214a5c
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-31 03:31:36 +0600

    guard user routes

--- src/modules/user/user_route.ts
+++ src/modules/user/user_route.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import authenticate from '../../middlewares/authenticate';
+import authorize from '../../middlewares/authorize';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user_controller';
 import {
@@ -13,32 +15,42 @@ const router = Router();
 
 router.post(
   '/',
+  authenticate,
+  authorize('user:create'),
   validateRequest(createUserValidationSchema),
   UserController.createUser,
 );
 
-router.get('/', UserController.getUsers);
+router.get('/', authenticate, authorize('user:read'), UserController.getUsers);
 
 router.patch(
   '/:id/status',
+  authenticate,
+  authorize('user:update'),
   validateRequest(updateUserStatusValidationSchema),
   UserController.updateUserStatus,
 );
 
 router.get(
   '/:id',
+  authenticate,
+  authorize('user:read'),
   validateRequest(getUserByIdValidationSchema),
   UserController.getUserById,
 );
 
 router.patch(
   '/:id',
+  authenticate,
+  authorize('user:update'),
   validateRequest(updateUserValidationSchema),
   UserController.updateUser,
 );
 
 router.delete(
   '/:id',
+  authenticate,
+  authorize('user:delete'),
   validateRequest(getUserByIdValidationSchema),
   UserController.deleteUser,
 );
