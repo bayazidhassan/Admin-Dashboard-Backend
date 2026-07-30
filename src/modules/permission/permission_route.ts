d25ff2135fb83e9f92a543ef14ec1811bc0f74fe
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-31 00:33:11 +0600

    protect permission route

--- src/modules/permission/permission_route.ts
+++ src/modules/permission/permission_route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import authenticate from '../../middlewares/authenticate';
+import authorize from '../../middlewares/authorize';
 import validateRequest from '../../middlewares/validateRequest';
 import { PermissionController } from './permission_controller';
 import {
@@ -13,17 +14,27 @@ const router = Router();
 router.post(
   '/groups',
   authenticate,
+  authorize('permission:create'),
   validateRequest(createGroupValidationSchema),
   PermissionController.createGroup,
 );
-router.get('/groups', PermissionController.getGroups);
+router.get(
+  '/groups',
+  authenticate,
+  authorize('permission:read'),
+  PermissionController.getGroups,
+);
 router.patch(
   '/groups/:id',
+  authenticate,
+  authorize('permission:update'),
   validateRequest(updateGroupValidationSchema),
   PermissionController.updateGroup,
 );
 router.delete(
   '/groups/:id',
+  authenticate,
+  authorize('permission:delete'),
   validateRequest(deletePermissionValidationSchema),
   PermissionController.deletePermission,
 );
